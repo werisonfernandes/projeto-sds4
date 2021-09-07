280601861e23b08fda379d0000c2439c5293eb41
Werison Aparecido Fernandes <werison.faw@gmail.com>
2021-09-07 12:20:07 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (<footer className="footer mt-auto py-3 bg-dark">
+        <div className="container">
+            <p className="text-light">App desenvolvido por <a href="https://www.linkedin.com/in/werison-aparecido-fernandes-538b52b8" target="_blank" rel="noreferrer">Werison Aparecido Fernandes</a></p>
+            <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+        </div>
+    </footer>);
+}
+
+export default Footer;
