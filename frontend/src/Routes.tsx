af6586944d199c8a22ff9ab561d138150fa65f26
Werison Aparecido Fernandes <werison.faw@gmail.com>
2021-09-10 14:59:40 -0300

    DonutChart integration

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -1,18 +1,18 @@
 import Dashboard from 'pages/Dashboard';
 import Home from 'pages/Home';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 const Routes = () => {
     return (
         <BrowserRouter>
-            <switch>
+            <Switch>
                 <Route path="/" exact>
                     <Home />
                 </Route>
                 <Route path="/dashboard">
                     <Dashboard />
                 </Route>
-            </switch>
+            </Switch>
         </BrowserRouter>
     );
 }
--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import { useEffect } from 'react';
+import { useState } from 'react';
 import Chart from 'react-apexcharts';
 import { SaleSum } from 'types/Sale';
 import { BASE_URL } from 'utils/requests';
@@ -10,40 +12,47 @@ type ChartData = {
 
 const DonutChart = () => {
 
-    //Forma errada
-    let chartData: ChartData = {
+    const [chartData, setChartData] = useState<ChartData>({
         labels: [],
         series: []
-    };
+    });
+
+    //Forma errada
+    /*let chartData: ChartData = {
+        labels: [],
+        series: []
+    };*/
 
     /*const mockData = {
         series: [477138, 499928, 444867, 220426, 473088],
         labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
     }*/
-    
+
     const options = {
         legend: {
             show: true
         }
     }
 
-    axios.get(`${BASE_URL}/sales/amount-by-seller`)
-        .then(response => {
-            const data = response.data as SaleSum[];
-            const myLabels = data.map(d => d.sellerName);
-            const mySeries = data.map(d => d.sum);
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales/amount-by-seller`)
+            .then(response => {
+                const data = response.data as SaleSum[];
+                const myLabels = data.map(d => d.sellerName);
+                const mySeries = data.map(d => d.sum);
 
-            chartData = {
-                labels: myLabels,
-                series: mySeries
-            };
+                setChartData({
+                    labels: myLabels,
+                    series: mySeries
+                });
 
-            console.log(chartData);
-            console.log(data);
-        }).catch(error => {
+                console.log(chartData);
+                console.log(data);
+            }).catch(error => {
 
-        });
-    
+            });
+    }, []);
+
     return (
         <Chart options={{ ...options, labels: chartData.labels }}
             series={chartData.series}
